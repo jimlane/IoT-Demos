be8e0e31a50796d2cd851684bea6069ad6e36894
Jim Lane <jimlane@microsoft.com>
2020-07-20 15:38:29 -0400

    add install script

---
+++ vm-templates/IotEdgeInstall1.ps1
@@ -0,0 +1,7 @@
+﻿# Check if OS version is supported, enable containers, download Moby
+. {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
+Deploy-IoTEdge -ContainerOs Windows
+
+# Configure/Initialize IoT Edge Runtime
+. {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
+Initialize-IoTEdge -ContainerOs Windows
